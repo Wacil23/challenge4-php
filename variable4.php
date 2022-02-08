33a5314e0a964cbf40bd8c9ba3ccdb0f436b291a
Wacil23 <wacilzek00@gmail.com>
2022-02-08 16:21:14 +0100

    finish

--- variable4.php
+++ variable4.php
@@ -1,1 +1,15 @@
 <?php
+    $movieOfStevenson = [
+        'JekyllNHyde' => ['Michael Caine', 'Cheryl Ladd', 'Joss Ackland'],
+        'TreasureIsland' => ['Robert Newton', 'Bobby Driscoll', 'Denis ODea'],
+        'Kidnapped' => ['Peter Finch', 'James MacArthur', 'John Laurie'],
+    ];
+
+    foreach($movieOfStevenson as $movieName => $actors){
+        echo (' Dans le film' . ' ' . $movieName . ' Les principales acteurs sont : ' );
+        echo '<br />';
+
+        foreach($actors as $actors){
+            echo ($actors . ', ');
+        }
+    }
